26777f2802e83919a8c1dd5c1670699e21143571
Danh2004 <anhvtdph40034@fpt.edu.vn>
2023-11-17 18:23:07 +0700

    bắt đầu dự án

--- app/src/main/java/com/example/nhom12_da1/Fragment/frag_ChuHang.java
+++ app/src/main/java/com/example/nhom12_da1/Fragment/frag_ChuHang.java
@@ -1,16 +1,4 @@
 package com.example.nhom12_da1.Fragment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-import com.example.nhom12_da1.R;
-
-public class frag_ChuHang extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_frag_chu_hang);
-    }
+public class frag_ChuHang {
 }
